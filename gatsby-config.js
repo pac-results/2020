436197523d7c57d2a54b2dcf56061ba532ed9d7d
andy-duncan <andy.duncan@tesglobal.com>
2020-03-02 17:02:32 +0200

    push the generated site for github-pages hosting

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pathPrefix: "/pac",
+  pathPrefix: "/2020",
   siteMetadata: {
     title: `PAC Race Results and Athlete Summaries`,
     description: `Pinelands Athletic Club Race Results and Athlete Summaries`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Layout from "../components/layout"
 
 const IndexPage = () => (
   <Layout>
-    <h1>PAC Results</h1>
+    <h1>PAC Results 2020</h1>
     <p><Link to="/athletes/">Athletes</Link></p>
     <p><Link to="/races/">Races</Link></p>
     <p><Link to="/monthly_reports/">Monthly Reports</Link></p>
